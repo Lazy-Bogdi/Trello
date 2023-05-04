c805578f8db8ff34b7e8b1eaa5918afee26230cf
Lazy_Dude <bmonote@normandiewebschool.fr>
2023-05-04 21:15:05 +0200

    correction contrainte

--- Trello/src/Entity/User.php
+++ Trello/src/Entity/User.php
@@ -37,7 +37,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
-    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Board::class)]
+    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Board::class, cascade: ['remove'])]
     private Collection $boards;
 
 
